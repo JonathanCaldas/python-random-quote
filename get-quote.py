9719e80173b2d91154adc0c6d89cce591992e2d8
JonathanCaldas <64319962+JonathanCaldas@users.noreply.github.com>
2020-04-25 18:10:25 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def testandoumavez():
+def testandoumavez1():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def testandoumavez():
   #print(quotes)
 
 if __name__== "__main__":
-  testandoumavez()
+  testandoumavez1()
